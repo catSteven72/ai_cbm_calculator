468422824c70f1d96e3213f849c17b8a44c383c3
User <samsebedrug@gmail.com>
2024-03-02 23:47:56 +0200

    v 1.0

--- cbm_ai_calc/cbm_ai_calc/urls.py
+++ cbm_ai_calc/cbm_ai_calc/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ai/', include('ai.urls'))
-]
+    path('ai/', include('ai.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
